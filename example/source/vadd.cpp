3764a8e6492a834f09207b6c74cc68bca8c6087e
BenQuickDeNN <2413269761@qq.com>
2020-06-03 19:13:13 +0800

    add vadd.cpp example

---
+++ example/source/vadd.cpp
@@ -0,0 +1,30 @@
+#include "intrinsics_vector.hpp"
+
+#include <cstdio>
+
+const int LEN = 16;
+
+float C[LEN], A[LEN], B[LEN];
+
+int main()
+{
+    using namespace std;
+
+    /* initialization */
+    for (int i = 0; i < LEN; i++)
+    {
+        A[i] = 2.0;
+        B[i] = 3.0;
+    }
+
+    Vecf va, vb;
+    va.load(A);
+    vb.load(B);
+
+    va = va + vb;
+    va.store(C);
+
+    for (int i = 0; i < LEN - 1; i++)
+        printf("%.1f\t", C[i]);
+    printf("%.1f\n", C[LEN - 1]);
+}
---
+++ intrinsics_vector.hpp
@@ -0,0 +1,12 @@
+/*************************************************
+ * @author Bin Qu
+ * @email benquickdenn@foxmail.com
+*************************************************/
+
+#ifndef INTRINSICS_VECTOR_HPP
+
+#define INTRINSICS_VECTOR_HPP
+
+#include "vec/vecf.hpp"
+
+#endif
